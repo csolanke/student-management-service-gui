555acad00fa43db96d37cb65c12f880b54ffd490
Chandrakant <csolanke77@gmail.com>
2021-11-29 11:18:12 -0500

    Added Header and Footer for student Management system GUI

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="footer">
+                <span className="text-muted">All the Rights are Resrved 2021 @CDS.org</span>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
