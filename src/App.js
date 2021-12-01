feaeb4ec4c7806022720682255b77de6a954a14d
Chandrakant <csolanke77@gmail.com>
2021-11-30 20:26:58 -0500

    Added CreateEmployeeComponent and respective Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -14,8 +15,9 @@ function App() {
 
         <div className="container">
               <Switch>
-                  <Route path="/" component={ListEmployeeComponent}></Route>
+                  <Route path="/" exact component={ListEmployeeComponent}></Route>
                   <Route path="/employees" component={ListEmployeeComponent}></Route>
+                  <Route path="/add-employee" component={CreateEmployeeComponent}></Route>
                </Switch>
         </div>
         <FooterComponent />
--- src/components/ListEmployeeComponent.js
+++ src/components/ListEmployeeComponent.js
@@ -3,54 +3,63 @@ import EmployeeService from '../services/EmployeeService';
 
 class ListEmployeeComponent extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
 
-        this.state ={
-            employees: [ ]
+        this.state = {
+            employees: []
         }
+
+        this.addEmployee = this.addEmployee.bind(this);
     };
 
 
-    componentDidMount()
-    {
-        EmployeeService.getEmployees().then((res)=>{
-            this.setState({employees: res.data});
+    componentDidMount() {
+        EmployeeService.getEmployees().then((res) => {
+            this.setState({ employees: res.data });
         })
+    }
+
+    addEmployee()
+    {
+       this.props.history.push('/add-employee'); 
     }
 
     render() {
         return (
             <div>
-               <h2 className="text-center">Employees List</h2>
+                <h2 className="text-center">Employees List</h2>
+                <div className="button">
+                    <button className="btn btn-primary" onClick={this.addEmployee}>Add Employee</button>
+                </div>
 
-               <div className="row">
+                <div className="row">
 
-                   <table className="table table-striped table-bordered">
-                   <thead>
-                        <tr>
-                            <th>Employee First Name</th>
-                            <th>Employee Last Name</th>
-                            <th>Employee Email Id</th>
-                            <th>Actions</th>
-                            
-                        </tr>
-                   </thead>
-                   <tbody>
-                       {this.state.employees.map(
-                           employee=>
-                           <tr key={employee.id}>
-                           <td>{employee.firstName}</td>
-                           <td>{employee.lastName}</td>
-                           <td>{employee.emailId}</td>
-                           </tr>
-                       )}
-                   </tbody>
-                   
-                   </table>
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Employee First Name</th>
+                                <th>Employee Last Name</th>
+                                <th>Employee Email Id</th>
+                                <th>Actions</th>
 
-               </div>
-                   
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.employees.map(
+                                employee =>
+                                    <tr key={employee.id}>
+                                        <td>{employee.firstName}</td>
+                                        <td>{employee.lastName}</td>
+                                        <td>{employee.emailId}</td>
+                                    </tr>
+                            )}
+                        </tbody>
+
+                    </table>
+
+                </div>
+
             </div>
         );
     }
